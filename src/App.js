6c1868d818af760fcb19396897dafcaa79d27680
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 16:12:59 -0500

    Add skeleton/test run of add new song button/functionality in Banner component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MusicTable from "./MusicTable/MusicTable";
+import Banner from "./Banner/Banner";
 
 function App() {
   return (
     <div>
       <MusicTable />
+      <Banner />
     </div>
   );
 }
---
+++ src/Banner/Banner.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
+const Banner = (props) => {
+
+    // const [newSong, setNewSong] = useState({})
+
+    // useEffect(() => {
+    //     makePostRequest();
+    // }, [])
+
+    async function makePostRequest(){
+        let newSong = {
+            title: "New Song 3",
+            album: "New Album",
+            artist: "New Artist",
+            genre: "New Genre",
+            releaseDate: "2022-02-02"
+        }
+
+        let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
+        if(response.status === 201){
+            console.log(response.data)
+        } else {
+            console.log('something did not work')
+        }
+    }
+
+    return ( 
+        <button onClick={makePostRequest}>add new song</button>
+     );
+}
+ 
+export default Banner;
