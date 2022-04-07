5b4cffbc131ef281d4a2d4ec3fb3051cfb033063
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 16:02:12 -0500

    Built out the music table component with a map of the api response that puts the songs into a table to display.

--- src/MusicTable/MusicTable.jsx
+++ src/MusicTable/MusicTable.jsx
@@ -1,7 +1,9 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from 'axios';
 
 const MusicTable = (props) => {
+
+    const [MusicLibrary, setMusicLibrary] = useState([])
 
     useEffect(() => {
         makeGetRequest();
@@ -10,18 +12,56 @@ const MusicTable = (props) => {
     async function makeGetRequest(){
         try{
             let response = await axios.get('http://127.0.0.1:8000/api/music/');
-            console.log(response.data);
+            setMusicLibrary(response.data)
         } catch (ex) {
             console.log('Oh no something didn\'t work right :(');
         }
     }
 
+    const DisplayMusic = () => {
+
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Release Date</th>
+                        <th>Genre</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {MusicLibrary.map((song, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{song.title}</td>
+                                <td>{song.artist}</td>
+                                <td>{song.album}</td>
+                                <td>{song.release_date}</td>
+                                <td>{song.genre}</td>
+                            </tr>
+                            )}   
+                        )
+                    }
+                </tbody>
+            </table>
+        );
+    }
+
     return ( 
         <div>
-            <h1>Practicing my async axios calls!</h1>
-            <button onClick={makeGetRequest}>Please work!!</button>
+            <h1>My Playlist</h1>
+            <DisplayMusic />
+            <button onClick={makeGetRequest}>Reload Playlist</button>
         </div>
+        
+
+        
      );
 }
  
 export default MusicTable;
+
+
