be114011e196dd569a12466f2e4a928c83d803a7
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 17:01:26 -0500

    Make form functional with inputs and makePostRequest. Move form from its own function into return element of Banner component.

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,38 +1,34 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import {Modal, Button} from "bootstrap";
 
 const Banner = (props) => {
 
-    // const [newSong, setNewSong] = useState({})
 
-    const AddNewSong = (props) => {
-        return (
-            <div>
-                <form>
-                    <label>Title</label>
-                    <input type='text' />
-                    <label>Artist</label>
-                    <input type='text' />
-                    <label>Album</label>
-                    <input type='text' />
-                    <label>Release Date</label>
-                    <input type='text' />
-                    <label>Genre</label>
-                    <input type='text' />
-                </form>
-            </div>
-        )
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [releaseDate, setReleaseDate] = useState('');
+    const [genre, setGenre] = useState('');
+
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            releaseDate: releaseDate,
+            genre: genre,
+        };
+        setTitle('')
+        setArtist('')
+        setAlbum('')
+        setReleaseDate('')
+        setGenre('')
+        makePostRequest(newSong)
     }
 
-    async function makePostRequest(){
-        let newSong = {
-            title: "New Song 3",
-            album: "New Album",
-            artist: "New Artist",
-            genre: "New Genre",
-            releaseDate: "2022-02-02"
-        }
+    async function makePostRequest(newSong){
 
         let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
         if(response.status === 201){
@@ -45,7 +41,21 @@ const Banner = (props) => {
     return ( 
         <div>
             <h1>My Playlist</h1>
-            <AddNewSong />
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input type='text' value={title} onChange={(event) => setTitle(event.target.value)}/>
+                    <label>Artist</label>
+                    <input type='text' value={artist} onChange={(event) => setArtist(event.target.value)} />
+                    <label>Album</label>
+                    <input type='text' value={album} onChange={(event) => setAlbum(event.target.value)} />
+                    <label>Release Date</label>
+                    <input type='text' value={releaseDate} onChange={(event) => setReleaseDate(event.target.value)} />
+                    <label>Genre</label>
+                    <input type='text' value={genre} onChange={(event) => setGenre(event.target.value)}/>
+                    <button type='submit'>Add</button>
+                </form>
+            </div>
         </div>
      );
 }
