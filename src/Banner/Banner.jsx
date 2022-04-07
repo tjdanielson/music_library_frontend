fd9e65533aec9b38d1e1111289176a1cf00a368d
tjdanielson <tessajdanielson@gmail.com>
2022-04-07 16:43:25 -0500

    Built out skeleton of form for adding a new song in Banner component.

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,13 +1,29 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
+import {Modal, Button} from "bootstrap";
 
 const Banner = (props) => {
 
     // const [newSong, setNewSong] = useState({})
 
-    // useEffect(() => {
-    //     makePostRequest();
-    // }, [])
+    const AddNewSong = (props) => {
+        return (
+            <div>
+                <form>
+                    <label>Title</label>
+                    <input type='text' />
+                    <label>Artist</label>
+                    <input type='text' />
+                    <label>Album</label>
+                    <input type='text' />
+                    <label>Release Date</label>
+                    <input type='text' />
+                    <label>Genre</label>
+                    <input type='text' />
+                </form>
+            </div>
+        )
+    }
 
     async function makePostRequest(){
         let newSong = {
@@ -29,9 +45,8 @@ const Banner = (props) => {
     return ( 
         <div>
             <h1>My Playlist</h1>
-            <button onClick={makePostRequest}>add new song</button>
+            <AddNewSong />
         </div>
-
      );
 }
  
