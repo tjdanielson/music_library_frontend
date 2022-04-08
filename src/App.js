e893da139a014783b9fe4cf57d944a8496f58ffa
tjdanielson <tessajdanielson@gmail.com>
2022-04-08 09:38:53 -0500

    Built out skeleton of searchbar component, imported and instantiated in app.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MusicTable from "./MusicTable/MusicTable";
 import Banner from "./Banner/Banner";
+import SearchBar from "./SearchBar/SearchBar";
 
 function App() {
   return (
     <div>
       <Banner />
+      <SearchBar />
       <MusicTable />
     </div>
   );
---
+++ src/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBar = (props) => {
+    
+    
+    return ( 
+        <div>
+            <h3>Search</h3>
+            <form>
+                <input type='text' />
+            </form>
+        </div>
+
+     );
+}
+ 
+export default SearchBar;
